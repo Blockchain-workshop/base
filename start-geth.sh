7a972eee573d15df7267549ec3ae7269b69fedc4
Steffen Hageland <steffen.hageland@bekk.no>
2016-06-09 16:52:59 +0200

    Add startupscript to launch geth on private blockchain

---
+++ start-geth.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+datadir="./datadir"
+genesisfile="genesis.json"
+networkid="123"
+rpcmodules="admin,miner,db,eth,debug,personal,web3"
+
+if [ ! -d "$DIRECTORY/chaindata" ]; then
+    geth --datadir $datadir --networkid $networkid init $genesisfile
+fi
+
+geth --datadir $datadir --networkid $networkid --rpc --rpcapi='$rpcmodules'
