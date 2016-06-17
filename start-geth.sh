ffeef9302c1e0168bbc57a7811e460c75b8294f2
Steffen Hageland <steffen.hageland@bekk.no>
2016-06-17 10:28:00 +0200

    Ikke unlock og lag account med en gang. La folk gjøre det manuelt

--- start-geth.sh
+++ start-geth.sh
@@ -13,8 +13,7 @@ rpcmodules="admin,miner,db,eth,debug,personal,web3,net"
 if [ ! -d "$datadir/chaindata" ]; then
     echo "initiate genesis-block"
     geth --datadir $datadir --networkid $networkid init $genesisfile
-    geth --datadir $datadir --networkid $networkid account new
 fi
 
 echo "launching geth"
-geth --datadir $datadir --networkid $networkid --rpc --rpcapi="$rpcmodules" --unlock 0 --rpccorsdomain "*" $@
+geth --datadir $datadir --networkid $networkid --rpc --rpcapi="$rpcmodules" --rpccorsdomain "*" $@
